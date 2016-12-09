3bd3f33fac6258e51a8c1c16a4f9e51d7005aa3e
Anthony Bottemiller <anthonybottemiller@outlook.com>
2016-12-08 20:23:01 -0800

    give Stylist object GetAll DeleteAll and Save

--- Objects/Stylist.cs
+++ Objects/Stylist.cs
@@ -1,3 +1,7 @@
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System;
+
 namespace HairSalon
 {
   public class Stylist
@@ -20,7 +24,74 @@ namespace HairSalon
     {
       return _id;
     }
-    
+
+    public static List<Stylist> GetAll()
+    {
+      List<Stylist> allStylists = new List<Stylist>{};
+
+      SqlConnection connection = DB.Connection();
+      connection.Open();
+      
+      SqlCommand command = new SqlCommand("SELECT * FROM stylists;", connection);      
+      SqlDataReader reader = command.ExecuteReader();
+
+      while(reader.Read())
+      {
+        int stylistId = reader.GetInt32(0);
+        string stylistName = reader.GetString(1);
+        Stylist newStylist = new Stylist(stylistName, stylistId);
+        allStylists.Add(newStylist);
+      }
+
+      if (reader != null)
+      {
+        reader.Close();
+      }
+
+      if (connection != null)
+      {
+        connection.Close();
+      }
+
+      return allStylists;
+    }
+
+    public void Save()
+    {
+      SqlConnection connection = DB.Connection();
+      connection.Open();
+
+      SqlCommand command = new SqlCommand("INSERT INTO stylists (name) OUTPUT INSERTED.id VALUES (@StylistName);", connection);
+
+      SqlParameter nameParameter = new SqlParameter();
+      nameParameter.ParameterName = "@StylistName";
+      nameParameter.Value = this.GetName();
+      command.Parameters.Add(nameParameter);
+      SqlDataReader reader = command.ExecuteReader();
+
+      while(reader.Read())
+      {
+        this._id = reader.GetInt32(0);
+      }
+      if (reader != null)
+      {
+        reader.Close();
+      }
+      if (connection != null)
+      {
+        connection.Close();
+      }
+    }
+
+public static void DeleteAll()
+    {
+      SqlConnection connection = DB.Connection();
+      connection.Open();
+      SqlCommand cmd = new SqlCommand("DELETE FROM stylists;", connection);
+      cmd.ExecuteNonQuery();
+      connection.Close();
+    }
+
     public override bool Equals(System.Object otherStylist)
     {
       if (!(otherStylist is Stylist))
