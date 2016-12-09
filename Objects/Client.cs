e761c9344925db1924b4da4bed9067bc7486d694
Anthony Bottemiller <anthonybottemiller@outlook.com>
2016-12-08 22:53:33 -0800

    implement GetAll() DeleteAll() and save() first test pass

--- Objects/Client.cs
+++ Objects/Client.cs
@@ -31,6 +31,82 @@ namespace HairSalon
     return _stylistId;
   }
 
+  public static List<Client> GetAll()
+  {
+    List<Client> allClients = new List<Client>{};
+
+    SqlConnection connection = DB.Connection();
+    connection.Open();
+    
+    SqlCommand command = new SqlCommand("SELECT * FROM clients;", connection);      
+    SqlDataReader reader = command.ExecuteReader();
+
+    while(reader.Read())
+    {
+      int clientId = reader.GetInt32(0);
+      int stylistId = reader.GetInt32(1);
+      string clientName = reader.GetString(2);
+      Client newClient = new Client(clientName, stylistId, clientId);
+      allClients.Add(newClient);
+    }
+
+    if (reader != null)
+    {
+      reader.Close();
+    }
+
+    if (connection != null)
+    {
+      connection.Close();
+    }
+
+    return allClients;
+  }
+
+  public void Save()
+  {
+    SqlConnection connection = DB.Connection();
+    connection.Open();
+
+    SqlCommand command = new SqlCommand("INSERT INTO clients (name, stylist_id) OUTPUT INSERTED.id VALUES (@ClientName, @StylistId);", connection);
+
+    SqlParameter nameParameter = new SqlParameter();
+    nameParameter.ParameterName = "@ClientName";
+    nameParameter.Value = this.GetName();
+    command.Parameters.Add(nameParameter);
+
+    SqlParameter idParameter = new SqlParameter();
+    idParameter.ParameterName = "@StylistId";
+    idParameter.Value = this.GetStylistId();
+    command.Parameters.Add(idParameter);
+
+    SqlDataReader reader = command.ExecuteReader();
+
+
+    while(reader.Read())
+    {
+      this._id = reader.GetInt32(0);
+    }
+    if (reader != null)
+    {
+      reader.Close();
+    }
+    if (connection != null)
+    {
+      connection.Close();
+    }
+  }
+
+  public static void DeleteAll()
+  {
+    SqlConnection connection = DB.Connection();
+    connection.Open();
+    SqlCommand cmd = new SqlCommand("DELETE FROM clients;", connection);
+    cmd.ExecuteNonQuery();
+    connection.Close();
+  }
+
+
   public override bool Equals(System.Object otherClient)
   {
     if (!(otherClient is Client))
