4d7ebe2af99846d3e2471ecfbc0d2134e4c65bba
Anthony Bottemiller <anthonybottemiller@outlook.com>
2016-12-08 19:21:52 -0800

    add test for equal

--- Tests/StylistTest.cs
+++ Tests/StylistTest.cs
@@ -12,7 +12,14 @@ namespace HairSalon
     {
       DBConfiguration.ConnectionString = "Data Source=(localdb)\\mssqllocaldb;Initial Catalog=hair_salon_test;Integrated Security=SSPI;";
     }
+    [Fact]
+    public void Stylist_Equal_ReturnsTrueIfNamesAreTheSame_True()
+    {
+      Stylist firstStylist = new Stylist("Justin Bryden");
+      Stylist secondStylist = new Stylist("Justin Bryden");
 
+      Assert.Equal(firstStylist, secondStylist);
+    }
     public void Dispose()
     {
       Stylist.DeleteAll();
