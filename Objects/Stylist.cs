4a5bfb83068ab72205e0a5423a71d88c2fb763b3
Anthony Bottemiller <anthonybottemiller@outlook.com>
2016-12-08 21:33:05 -0800

    implements update for stylists. passes test

--- Objects/Stylist.cs
+++ Objects/Stylist.cs
@@ -15,148 +15,182 @@ namespace HairSalon
     _name = Name;
   }
 
-    public string GetName()
+  public string GetName()
+  {
+    return _name;
+  }
+
+  public int GetId()
+  {
+    return _id;
+  }
+
+  public static Stylist Find(int id)
+  {
+    SqlConnection connection = DB.Connection();
+    connection.Open();
+
+    SqlCommand command = new SqlCommand("SELECT * FROM stylists WHERE id = @StylistId;", connection);
+    SqlParameter stylistIdParameter = new SqlParameter();
+    stylistIdParameter.ParameterName = "@StylistId";
+    stylistIdParameter.Value = id.ToString();
+    command.Parameters.Add(stylistIdParameter);
+    SqlDataReader reader = command.ExecuteReader();
+
+    int foundStylistId = 0;
+    string foundStylistName = null;
+
+    while(reader.Read())
     {
-      return _name;
+      foundStylistId = reader.GetInt32(0);
+      foundStylistName = reader.GetString(1);
     }
 
-    public int GetId()
+    Stylist foundStylist = new Stylist(foundStylistName, foundStylistId);
+
+    if (reader != null)
     {
-      return _id;
+      reader.Close();
     }
 
-    public static Stylist Find(int id)
+    if (connection != null)
     {
-      SqlConnection connection = DB.Connection();
-      connection.Open();
-
-      SqlCommand command = new SqlCommand("SELECT * FROM stylists WHERE id = @StylistId;", connection);
-      SqlParameter stylistIdParameter = new SqlParameter();
-      stylistIdParameter.ParameterName = "@StylistId";
-      stylistIdParameter.Value = id.ToString();
-      command.Parameters.Add(stylistIdParameter);
-      SqlDataReader reader = command.ExecuteReader();
-
-      int foundStylistId = 0;
-      string foundStylistName = null;
-
-      while(reader.Read())
-      {
-        foundStylistId = reader.GetInt32(0);
-        foundStylistName = reader.GetString(1);
-      }
-
-      Stylist foundStylist = new Stylist(foundStylistName, foundStylistId);
-
-      if (reader != null)
-      {
-        reader.Close();
-      }
-
-      if (connection != null)
-      {
-        connection.Close();
-      }
-      return foundStylist;
-
+      connection.Close();
     }
-    public static List<Stylist> GetAll()
-    {
-      List<Stylist> allStylists = new List<Stylist>{};
-
-      SqlConnection connection = DB.Connection();
-      connection.Open();
-      
-      SqlCommand command = new SqlCommand("SELECT * FROM stylists;", connection);      
-      SqlDataReader reader = command.ExecuteReader();
-
-      while(reader.Read())
-      {
-        int stylistId = reader.GetInt32(0);
-        string stylistName = reader.GetString(1);
-        Stylist newStylist = new Stylist(stylistName, stylistId);
-        allStylists.Add(newStylist);
-      }
-
-      if (reader != null)
-      {
-        reader.Close();
-      }
-
-      if (connection != null)
-      {
-        connection.Close();
-      }
-
-      return allStylists;
+    return foundStylist;
     }
 
-    public void Save()
+  public static List<Stylist> GetAll()
+  {
+    List<Stylist> allStylists = new List<Stylist>{};
+
+    SqlConnection connection = DB.Connection();
+    connection.Open();
+    
+    SqlCommand command = new SqlCommand("SELECT * FROM stylists;", connection);      
+    SqlDataReader reader = command.ExecuteReader();
+
+    while(reader.Read())
     {
-      SqlConnection connection = DB.Connection();
-      connection.Open();
-
-      SqlCommand command = new SqlCommand("INSERT INTO stylists (name) OUTPUT INSERTED.id VALUES (@StylistName);", connection);
-
-      SqlParameter nameParameter = new SqlParameter();
-      nameParameter.ParameterName = "@StylistName";
-      nameParameter.Value = this.GetName();
-      command.Parameters.Add(nameParameter);
-      SqlDataReader reader = command.ExecuteReader();
-
-      while(reader.Read())
-      {
-        this._id = reader.GetInt32(0);
-      }
-      if (reader != null)
-      {
-        reader.Close();
-      }
-      if (connection != null)
-      {
-        connection.Close();
-      }
+      int stylistId = reader.GetInt32(0);
+      string stylistName = reader.GetString(1);
+      Stylist newStylist = new Stylist(stylistName, stylistId);
+      allStylists.Add(newStylist);
     }
 
-public static void Delete(int id)
+    if (reader != null)
     {
-      SqlConnection connection = DB.Connection();
-      connection.Open();
-      SqlCommand command = new SqlCommand("DELETE FROM stylists WHERE id = @StylistId;", connection);
-      SqlParameter stylistIdParameter = new SqlParameter();
-      stylistIdParameter.ParameterName = "@StylistId";
-      stylistIdParameter.Value = id.ToString();
-      command.Parameters.Add(stylistIdParameter);
-      command.ExecuteNonQuery();
+      reader.Close();
+    }
+
+    if (connection != null)
+    {
       connection.Close();
     }
 
-public static void DeleteAll()
+    return allStylists;
+  }
+
+  public void Save()
+  {
+    SqlConnection connection = DB.Connection();
+    connection.Open();
+
+    SqlCommand command = new SqlCommand("INSERT INTO stylists (name) OUTPUT INSERTED.id VALUES (@StylistName);", connection);
+
+    SqlParameter nameParameter = new SqlParameter();
+    nameParameter.ParameterName = "@StylistName";
+    nameParameter.Value = this.GetName();
+    command.Parameters.Add(nameParameter);
+    SqlDataReader reader = command.ExecuteReader();
+
+    while(reader.Read())
     {
-      SqlConnection connection = DB.Connection();
-      connection.Open();
-      SqlCommand cmd = new SqlCommand("DELETE FROM stylists;", connection);
-      cmd.ExecuteNonQuery();
+      this._id = reader.GetInt32(0);
+    }
+    if (reader != null)
+    {
+      reader.Close();
+    }
+    if (connection != null)
+    {
       connection.Close();
     }
+  }
 
-    public override bool Equals(System.Object otherStylist)
+  public void Update(string newName)
+  {
+    SqlConnection connection = DB.Connection();
+    connection.Open();
+
+    SqlCommand command = new SqlCommand("UPDATE stylists SET name = @NewName OUTPUT INSERTED.name WHERE id = @StylistId;", connection);
+    
+    SqlParameter newNameParameter = new SqlParameter();
+    newNameParameter.ParameterName = "@NewName";
+    newNameParameter.Value = newName;
+    command.Parameters.Add(newNameParameter);
+
+    SqlParameter stylistIdParameter = new SqlParameter();
+    stylistIdParameter.ParameterName = "@StylistId";
+    stylistIdParameter.Value = this.GetId();
+    command.Parameters.Add(stylistIdParameter);
+    SqlDataReader reader = command.ExecuteReader();
+    
+    while(reader.Read())
     {
-      if (!(otherStylist is Stylist))
-      {
-        return false;
-      }
-      else
-      {
-        Stylist newStylist = (Stylist) otherStylist;
-        bool idEquality = this.GetId() == newStylist.GetId();
-        bool nameEquality = this.GetName() == newStylist.GetName();
-        return (nameEquality && idEquality);
-      }
+      this._name = reader.GetString(0);
     }
-    public override int GetHashCode()
+
+    if (reader != null)
+    {
+      reader.Close();
+    }
+
+    if (connection != null)
+    {
+      connection.Close();
+    }
+  }
+
+  public static void Delete(int id)
+  {
+    SqlConnection connection = DB.Connection();
+    connection.Open();
+    SqlCommand command = new SqlCommand("DELETE FROM stylists WHERE id = @StylistId;", connection);
+    SqlParameter stylistIdParameter = new SqlParameter();
+    stylistIdParameter.ParameterName = "@StylistId";
+    stylistIdParameter.Value = id.ToString();
+    command.Parameters.Add(stylistIdParameter);
+    command.ExecuteNonQuery();
+    connection.Close();
+  }
+
+  public static void DeleteAll()
+  {
+    SqlConnection connection = DB.Connection();
+    connection.Open();
+    SqlCommand cmd = new SqlCommand("DELETE FROM stylists;", connection);
+    cmd.ExecuteNonQuery();
+    connection.Close();
+  }
+
+      public override bool Equals(System.Object otherStylist)
+      {
+        if (!(otherStylist is Stylist))
         {
-            return this.GetName().GetHashCode();
+          return false;
         }
+        else
+        {
+          Stylist newStylist = (Stylist) otherStylist;
+          bool idEquality = this.GetId() == newStylist.GetId();
+          bool nameEquality = this.GetName() == newStylist.GetName();
+          return (nameEquality && idEquality);
+        }
+      }
+  public override int GetHashCode()
+  {
+    return this.GetName().GetHashCode();
+  }
   }
 }
